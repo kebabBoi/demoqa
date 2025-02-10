0bc72982197153e734363257165b1cb63243da8e
Dmitriy Gochev <gematogen2002@gmail.com>
2025-02-10 12:08:24 +0300

    first tests

---
+++ tests/test_check_icon_demoqa.py
@@ -0,0 +1,22 @@
+# from selenium.webdriver.common.by import By
+from pages.demoqa import DemoQa
+import time
+
+# def test_check_icon(browser):
+    # browser.get('https://demoqa.com/')
+    #
+    # icon = browser.find_element(By.CSS_SELECTOR, '#app > header > a')
+    #
+    # if icon is None:
+    #     print('Не найден элемент')
+    # else:
+    #     print('Элемент найден')
+
+def test_check_icon(browser):
+    demo_qa_page = DemoQa(browser)
+    demo_qa_page.visit()
+    # time.sleep(3)
+    demo_qa_page.click_on_the_icon()
+    # time.sleep(3)
+    assert demo_qa_page.equal_url()
+    assert demo_qa_page.exist_icon()
